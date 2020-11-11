5500d2ffe401cd9ee4b8fc2f0dab5f64672b962c
mancha-heinz <manchaheinz@gmail.com>
2020-11-11 08:59:46 -0300

    testando a primeira classe de dominio - Cidade

--- src/main/java/com/gabriel/desafiolivelo/resources/CidadeResource.java
+++ src/main/java/com/gabriel/desafiolivelo/resources/CidadeResource.java
@@ -1,15 +1,27 @@
 package com.gabriel.desafiolivelo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabriel.desafiolivelo.domain.Cidade;
+
 @RestController
 @RequestMapping(value = "/cidades")
 public class CidadeResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listart() {
-		return "rest esta funcionando!";
+	public List<Cidade> listart() {
+		Cidade cid1 = new Cidade(1, "passo fundo", "rs");
+		Cidade cid2 = new Cidade(2, "marau", "rs");
+		
+		List<Cidade> lista=new ArrayList<>();
+		lista.add(cid1);
+		lista.add(cid2);
+		
+		return lista;
 	}
 }
