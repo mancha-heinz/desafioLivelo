043fae948aa856523272779eb1b7da4b91a5867d
mancha-heinz <manchaheinz@gmail.com>
2020-11-11 08:30:50 -0300

    Projeto criado

---
+++ src/main/java/com/gabriel/desafiolivelo/DesafioliveloApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.desafiolivelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioliveloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioliveloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/desafiolivelo/DesafioliveloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.desafiolivelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioliveloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
